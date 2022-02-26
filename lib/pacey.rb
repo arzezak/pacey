6c0219fb7a2dfdf518609a91b2a6e0e13b62324b
Ariel Rzezak <arzezak@gmail.com>
2022-02-26 11:03:15 -0300

    Initialization

---
+++ lib/pacey.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "pacey/version"
+
+module Pacey
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/pacey/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Pacey
+  VERSION = "0.1.0"
+end
